dcca18c9266aafb5bc5a8caf1e8f8e959adee607
david <dsissoko@gmail.com>
2025-06-30 16:35:47 +0200

    fixed unit test

--- src/test/java/com/r3edge/task/dispatcher/DispatcherIntegrationTest.java
+++ src/test/java/com/r3edge/task/dispatcher/DispatcherIntegrationTest.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+@SpringBootTest(classes = TestApplication.class)
 public class DispatcherIntegrationTest {
 
     @Autowired
---
+++ src/test/java/com/r3edge/task/dispatcher/TestApplication.java
@@ -0,0 +1,7 @@
+package com.r3edge.task.dispatcher;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApplication {
+}
